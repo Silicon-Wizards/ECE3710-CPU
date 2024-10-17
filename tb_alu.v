b0b261372dfa330b599dcb4b08350f59c4d70e41
cloudjumper001 <personal+github_cloudjumper001@foxhaven.space>
2024-10-17 17:45:38 -0600

    made logic tests for alu.
    made logic tests for alu.
    
    CONTROL_LSH is broken. see discord for the error

--- tb_alu.v
+++ tb_alu.v
@@ -3,15 +3,16 @@ module tb_alu;
 	parameter WIDTH_DATA = 16;
 	parameter WIDTH_CONTROL = 4;
 	
-	parameter CONTROL_ADD =	'b0;
-	parameter CONTROL_SUB =	'b1;
-	parameter CONTROL_CMP =	'b10;
-	parameter CONTROL_AND =	'b11;
-	parameter CONTROL_OR =	'b100;
+	parameter CONTROL_ADD =	'b000;
+	parameter CONTROL_SUB =	'b001;
+	parameter CONTROL_CMP =	'b010;
+	parameter CONTROL_AND =	'b011;
+	parameter CONTROL_OR  =	'b100;
 	parameter CONTROL_XOR =	'b101;
 	parameter CONTROL_LSH =	'b110;
 	//tb_parameters
 	integer i;
+	integer j;
 	
 	//input [WIDTH_DATA - 1 : 0] A, B,
 	//input [WIDTH_CONTROL - 1 : 0] control_word,
@@ -32,14 +33,97 @@ module tb_alu;
 		.A(tb_A),
 		.B(tb_B),
 		.control_word(tb_control_word),
-		.carry_in(tb_carry_in)
+		.carry_in(tb_carry_in),
+		.result(tb_result),
+		.carry_out(tb_carry_out),
+		.zero_out(tb_zero_out)
 	);
 	
 	initial begin
-		//#5;
-		//tb_control_word = CONTROL_ADD;
-		//tb_A = 5;
-		//tb_B = 6;
-		//#5;
+	/**
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADD;
+		for(i = -4; i < 4; i = i + 1) begin
+			for(j = -4; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+			end
+		end
+		
+		tb_carry_in = 1;
+		tb_control_word = CONTROL_ADD;
+		for(i = -4; i < 4; i = i + 1) begin
+			for(j = -4; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+			end
+		end
+		**/
+		
+		/**
+		$display("TEST: and")
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_AND;
+		for(i = 0; i < 4; i = i + 1) begin
+			for(j = 0; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i & j));
+			end
+		end
+		$display("");
+		$display("");
+		**/
+		
+		/**
+		$display("TEST OR");
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_OR;
+		for(i = 0; i < 4; i = i + 1) begin
+			for(j = 0; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i | j));
+			end
+		end
+		$display("");
+		$display("");
+		**/
+		
+		
+		$display("TEST XOR");
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_XOR;
+		for(i = 0; i < 4; i = i + 1) begin
+			for(j = 0; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i ^ j));
+			end
+		end
+		$display("");
+		$display("");
+		
+		
+		$display("TEST LSH");
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_LSH;
+		for(i = 0; i < 4; i = i + 1) begin
+			for(j = 0; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i << j));
+			end
+		end
+		$display("");
+		$display("");
+		
+
 	end
 endmodule
