fa7934d92eec6efaec76d11c5929e59b9b33c578
encoon <ihlee0519@gmail.com>
2024-10-17 17:35:43 -0600

    Finished datapath with current ALU. Flags for ALU are not set yet.

--- cpu.v
+++ cpu.v
@@ -14,16 +14,20 @@ module cpu #(
 	parameter FILE_LOCATION = "../reg.dat"
 )(
 	input clk, reset,
-	output [REG_WIDTH-1:0] regALUoutput
+	input [4:0] aluControl,
+	input [REG_ADDR_BITS-1:0] address1, address2,
+	
+	output [REG_WIDTH-1:0] regALUoutput,
+	output carry, low, flag, zero, negative,
+	output [6:0] sevenSeg1, sevenSeg2, sevenSeg3, sevenSeg4
 );
 
 	// Register datapath variables.
 	wire regWriteEnable;
-	wire [REG_ADDR_BITS-1:0] regAddress1, regAddress2;
-	wire [REG_WIDTH-1:0] regWriteData, regReadData1, regReadData2;
+	wire [REG_WIDTH-1:0] regReadData1, regReadData2;
 	
 	// Instantiate the register file.
-	registerFile #(REG_WIDTH, REG_ADDR_BITS, FILE_LOCATION) registers(clk, regWriteEnable, regAddress1, regAddress2, regWriteData, regReadData1, regReadData2);
+	registerFile #(REG_WIDTH, REG_ADDR_BITS, FILE_LOCATION) registers(clk, regWriteEnable, srcAddress, dstAddress, aluOutput, regReadData1, regReadData2);
 	
 	
 
@@ -31,9 +35,22 @@ module cpu #(
 	wire [REG_WIDTH-1:0] pcIn;
 	wire [REG_WIDTH-1:0]	pcOut;
 	
-	flopenr #(REG_WIDTH) programCounter(clk, reset, progCountEnable, pcIn, pcOut);
+	flopenr #(REG_WIDTH) programCounter(clk, reset, progCountEnable, aluOutput, pcOut);
+	
+
+	
+	wire srcAddressEnable;
+	wire [REG_WIDTH-1:0] srcAddress;
+	
+	flopenr2 #(REG_WIDTH) srcAddressReg(clk, reset, srcAddressEnable, address1, srcAddress);
 	
 	
+	wire dstAddressEnable;
+	wire [REG_WIDTH-1:0] dstAddress;
+	
+	flopenr2 #(REG_WIDTH) dstAddressReg(clk, reset, dstAddressEnable, address2, dstAddress); 
+	
+
 	
 	wire immediateEnable;
 	wire [REG_WIDTH-1:0] immIn;
@@ -44,34 +61,37 @@ module cpu #(
 	
 	
 	wire mux_1Select;
-	wire [REG_WIDTH-1:0] mux_1Out; // change var name
+	wire [REG_WIDTH-1:0] aluInputA;
 	
-	mux2 #(REG_WIDTH) mux_1(mux_1Select, pcOut, regReadData1, mux_1Out);
+	mux2 #(REG_WIDTH) mux_1(mux_1Select, pcOut, regReadData1, aluInputA);
 	
 	
 	
 	
 	wire mux_2Select;
-	wire [REG_WIDTH-1:0] mux_2Out; // change var name
+	wire [REG_WIDTH-1:0] aluInputB;
 	
-	mux2 #(REG_WIDTH) mux_2(mux_2Select, regReadData2, immOut, mux_1Out);
-	
-	
-	
-	wire [REG_WIDTH-1:0] aluControl
-	wire [REG_WIDTH-1:0] aluResult 	// ALU output
-	wire aluCarryOut, aluZeroOut
-	
-	alu #(REG_WIDTH, REG_ADDR_BITS) alu(mux_1Out, mux_2Out, aluControl, carry_in, aluResult, aluCarryOut, aluZeroOut)
+	mux2 #(REG_WIDTH) mux_2(mux_2Select, regReadData2, immOut, aluInputB);
 	
 	
 	
+	wire [REG_WIDTH-1:0] aluOutput;
+	wire aluCarryIn, aluCarryOut, aluZeroOut;
+	
+	alu #(REG_WIDTH, REG_ADDR_BITS) alu(aluInputA, aluInputB, aluControl, aluCarryIn, aluOutput, aluCarryOut, aluZeroOut);
+	
+	
+		
 	wire regALUEnable;
 	
-	flopenr #(REG_WIDTH) regALU(clk, reset, regALUEnable, aluResult, regALUoutput);
+	flopenr #(REG_WIDTH) regALU(clk, reset, regALUEnable, aluOutput, regALUoutput);
 	
 	
-
+	hexTo7Seg sevenSegConverter1(regALUoutput[3:0], sevenSeg1);
+	hexTo7Seg sevenSegConverter2(regALUoutput[7:4], sevenSeg2);
+	hexTo7Seg sevenSegConverter3(regALUoutput[11:8], sevenSeg3);
+	hexTo7Seg sevenSegConverter4(regALUoutput[15:12], sevenSeg4);
 	
+		
 	
 endmodule
