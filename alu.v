702db69e9b9799cacbe714eb12d1637731d1ee3c
AdrianSucahyo <adrianpsucahyo@gmail.com>
2024-10-17 16:51:16 -0600

    Flags added to ALU

--- alu.v
+++ alu.v
@@ -16,36 +16,45 @@ module alu #(
 	input [WIDTH_CONTROL - 1 : 0] control_word,
 	input carry_in,
 	output reg [WIDTH_DATA - 1 : 0] result,
-	output reg carry_out, zero_out
+	output reg carry_out, low_out, over_out, zero_out, neg_out 
 );
 
-	parameter CONTROL_ADD =	'b0;
-	parameter CONTROL_SUB =	'b1;
-	parameter CONTROL_CMP =	'b10;
-	parameter CONTROL_AND =	'b11;
-	parameter CONTROL_OR =	'b100;
-	parameter CONTROL_XOR =	'b101;
-	parameter CONTROL_LSH =	'b110;
+	parameter CONTROL_ADD 	=	'b0;
+	parameter CONTROL_ADDU	=	'b1;
+	parameter CONTROL_SUB 	=	'b10;
+	parameter CONTROL_SUBU	=	'b11;
+	parameter CONTROL_CMP 	=	'b100;
+	parameter CONTROL_AND 	=	'b101;
+	parameter CONTROL_OR 	=	'b110;
+	parameter CONTROL_XOR	=	'b111;
+	parameter CONTROL_LSH 	=	'b1000;
 	
 	reg [WIDTH_DATA - 1: 0] shift_wire; 
 	
 	always @(*) 
 		begin
 		// Set the default flags
-		zero_out <= 0;
 		carry_out <= 0;
+		low_out <= 0;
+		zero_out <= 0;
+		over_out <= 0;
+		neg_out <= 0;
+		
 		shift_wire <= 0;
 		
 		case (control_word)
 			CONTROL_ADD : begin
 				{carry_out, result} <= A + B + carry_in;
 			end
+			CONTROL_ADDU : begin
+				{carry_out, result} <= A + B + carry_in;
+				over_out <= carry_out;
+			end
 			CONTROL_SUB : begin
 				result <= A - B;
 			end
 			CONTROL_CMP : begin
-				result <= B - A;
-				zero_out <= (result == 0) ? 1'b0 : 1'b0;
+				neg_out <= (B - A)[WIDTH_DATA - 1] == 'b1;
 			end
 			CONTROL_AND : begin
 				result <= A & B;
@@ -68,6 +77,8 @@ module alu #(
 			end
 			default: begin result <= 0; end
 		endcase
+		
+		assign zero <= result == 0 ? 'b1 : 1'b0;
 	end
 	
 endmodule
