2ecb62f99a973bad3bbdee749d4f64ba48d8fdfe
cloudjumper001 <personal+github_cloudjumper001@foxhaven.space>
2024-10-19 20:33:08 -0600

    Update tb_alu.v
    added more tests for ZF, CF, OF

--- tb_alu.v
+++ tb_alu.v
@@ -3,13 +3,15 @@ module tb_alu;
 	parameter WIDTH_DATA = 16;
 	parameter WIDTH_CONTROL = 4;
 	
-	parameter CONTROL_ADD =	'b000;
-	parameter CONTROL_SUB =	'b001;
-	parameter CONTROL_CMP =	'b010;
-	parameter CONTROL_AND =	'b011;
-	parameter CONTROL_OR  =	'b100;
-	parameter CONTROL_XOR =	'b101;
-	parameter CONTROL_LSH =	'b110;
+	localparam CONTROL_ADD 	=	'b0;
+	localparam CONTROL_ADDU	=	'b1;
+	localparam CONTROL_SUB 	=	'b10;
+	localparam CONTROL_SUBU	=	'b11;
+	localparam CONTROL_CMP 	=	'b100;
+	localparam CONTROL_AND 	=	'b101;
+	localparam CONTROL_OR 	=	'b110;
+	localparam CONTROL_XOR	=	'b111;
+	localparam CONTROL_LSH 	=	'b1000;
 	//tb_parameters
 	integer i;
 	integer j;
@@ -27,6 +29,9 @@ module tb_alu;
 	
 	wire [WIDTH_DATA - 1 : 0] 		tb_result;			//output reg [WIDTH_DATA - 1 : 0] result
 	wire 									tb_carry_out;		//output reg carry_out, zero_out
+	wire 									tb_low_out;
+	wire									tb_over_out;
+	wire									tb_neg_out;
 	wire 									tb_zero_out;		//output reg carry_out, zero_out
 
 	alu #(WIDTH_DATA, WIDTH_CONTROL) DUT(
@@ -36,34 +41,15 @@ module tb_alu;
 		.carry_in(tb_carry_in),
 		.result(tb_result),
 		.carry_out(tb_carry_out),
+		.low_out(tb_low_out),
+		.over_out(tb_over_out),
+		.neg_out(tb_neg_out),
 		.zero_out(tb_zero_out)
 	);
 	
 	initial begin
-	/**
-		tb_carry_in = 0;
-		tb_control_word = CONTROL_ADD;
-		for(i = -4; i < 4; i = i + 1) begin
-			for(j = -4; j < 4; j = j + 1) begin
-				tb_A = i;
-				tb_B = j;
-				#5;
-			end
-		end
-		
-		tb_carry_in = 1;
-		tb_control_word = CONTROL_ADD;
-		for(i = -4; i < 4; i = i + 1) begin
-			for(j = -4; j < 4; j = j + 1) begin
-				tb_A = i;
-				tb_B = j;
-				#5;
-			end
-		end
-		**/
-		
 		/**
-		$display("TEST: and")
+		$display("TEST: and");
 		tb_carry_in = 0;
 		tb_control_word = CONTROL_AND;
 		for(i = 0; i < 4; i = i + 1) begin
@@ -71,14 +57,12 @@ module tb_alu;
 				tb_A = i;
 				tb_B = j;
 				#5;
-				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i & j));
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i[15:0] & j[15:0]));
 			end
 		end
 		$display("");
 		$display("");
-		**/
 		
-		/**
 		$display("TEST OR");
 		tb_carry_in = 0;
 		tb_control_word = CONTROL_OR;
@@ -87,12 +71,11 @@ module tb_alu;
 				tb_A = i;
 				tb_B = j;
 				#5;
-				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i | j));
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i[15:0] | j[15:0]));
 			end
 		end
 		$display("");
 		$display("");
-		**/
 		
 		
 		$display("TEST XOR");
@@ -103,7 +86,7 @@ module tb_alu;
 				tb_A = i;
 				tb_B = j;
 				#5;
-				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i ^ j));
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i[15:0] ^ j[15:0]));
 			end
 		end
 		$display("");
@@ -118,12 +101,171 @@ module tb_alu;
 				tb_A = i;
 				tb_B = j;
 				#5;
-				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i << j));
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i[15:0] << j[15:0]));
 			end
 		end
 		$display("");
 		$display("");
 		
-
+		
+		$display("TEST ADDU");
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADDU;
+		for(i = 0; i < 4; i = i + 1) begin
+			for(j = 0; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i[15:0] + j[15:0]));
+			end
+		end
+		$display("");
+		$display("");
+		
+		$display("TEST ADD");
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADD;
+		for(i = -4; i < 4; i = i + 1) begin
+			for(j = -4; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i[15:0] + j[15:0]));
+			end
+		end
+		$display("");
+		$display("");
+		
+		$display("TEST SUBU");
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_SUBU;
+		for(i = 0; i < 4; i = i + 1) begin
+			for(j = 0; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i[15:0] - j[15:0]));
+			end
+		end
+		$display("");
+		$display("");
+		
+		$display("TEST SUB");
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_SUB;
+		for(i = -4; i < 4; i = i + 1) begin
+			for(j = -4; j < 4; j = j + 1) begin
+				tb_A = i;
+				tb_B = j;
+				#5;
+				$display("i: %h \t j: %h \t result: %h, \t should be: %h", i, j, tb_result, (i[15:0] - j[15:0]));
+			end
+		end
+		$display("");
+		$display("");
+		**/
+		
+		$display("TEST CF- unsigned overflow"); //---------------------------------------------------------------------------------------------------------
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADDU;
+		tb_A = 16'hFFFF; 
+		tb_B = 16'h0001;
+		#5;
+		if(tb_carry_out != 1) $display("ERROR!! CF flag: %h not set correctly should be 1", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADDU;
+		tb_A = 16'hFFFE; 
+		tb_B = 16'h0001;
+		#5;
+		if(tb_carry_out != 0) $display("ERROR!! CF flag: %h not set correctly should be 0", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_SUBU;
+		tb_A = 16'h0000; 
+		tb_B = 16'h0001;
+		#5;
+		if(tb_carry_out != 1) $display("ERROR!! CF flag: %h not set correctly should be 1", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_SUBU;
+		tb_A = 16'h0001; 
+		tb_B = 16'h0001;
+		#5;
+		if(tb_carry_out != 0) $display("ERROR!! CF flag: %h not set correctly should be 0", tb_carry_out);
+		
+		$display("TEST OF- signed overflow"); //------------------------------------------------------------------------------------------------------------
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADD;
+		tb_A = 16'h7FFF;
+		tb_B = 16'h0001;
+		#5;
+		if(tb_over_out != 1) $display("ERROR!! OF flag: %h not set correctly should be 1", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADD;
+		tb_A = 16'h7FFE;
+		tb_B = 16'h0001;
+		#5;
+		if(tb_over_out != 0) $display("ERROR!! OF flag: %h not set correctly should be 0", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_SUB;
+		tb_A = 16'h8000;
+		tb_B = 16'h0001;
+		#5;
+		if(tb_over_out != 1) $display("ERROR!! OF flag: %h not set correctly should be 1", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_SUB;
+		tb_A = 16'h8001;
+		tb_B = 16'h0001;
+		#5;
+		if(tb_over_out != 0) $display("ERROR!! OF flag: %h not set correctly should be 0", tb_carry_out);
+		
+		/**
+		$display("TEST ZF- signed overflow"); //---------------------------------------------------------------------------
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADDU;
+		tb_A = 16'h0000;
+		tb_B = 16'h0000;
+		#5;
+		if(tb_zero_out != 1) $display("ERROR!! ZF flag: %h not set correctly should be 1", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADDU;
+		tb_A = 16'h0001;
+		tb_B = 16'h0000;
+		#5;
+		if(tb_zero_out != 0) $display("ERROR!! ZF flag: %h not set correctly should be 0", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADDU;
+		tb_A = 16'h0000;
+		tb_B = 16'h0001;
+		#5;
+		if(tb_zero_out != 0) $display("ERROR!! ZF flag: %h not set correctly should be 0", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADD;
+		tb_A = 16'h0000;
+		tb_B = 16'h0000;
+		#5;
+		if(tb_zero_out != 1) $display("ERROR!! ZF flag: %h not set correctly should be 1", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADD;
+		tb_A = 16'h0001;
+		tb_B = 16'h0000;
+		#5;
+		if(tb_zero_out != 0) $display("ERROR!! ZF flag: %h not set correctly should be 0", tb_carry_out);
+		
+		tb_carry_in = 0;
+		tb_control_word = CONTROL_ADD;
+		tb_A = 16'h0000;
+		tb_B = 16'h0001;
+		#5;
+		if(tb_zero_out != 0) $display("ERROR!! ZF flag: %h not set correctly should be 0", tb_carry_out);
+		**/
 	end
 endmodule
